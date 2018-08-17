e479cfd58ed0e2747c50e66c3401b52e78460598
Viktor Sivulskiy <svicvg@gmail.com>
2018-08-17 10:26:38 +0600

    A apply.js File Was Added

---
+++ src/apply.js
@@ -0,0 +1,21 @@
+let employee1 = {
+    name: 'Hanna',
+    position: 'Project manager',
+    salary: 1000
+}
+
+let employee2 = {
+    name: 'Bill',
+    position: 'Junior developer',
+    salary: 800
+}
+
+function promote (newPosition, salaryRise){
+    this.position=newPosition;
+    this.salary+=salaryRise;
+
+    return `${this.name} is ${this.position} and earns $${this.salary}`;
+}
+
+console.log(promote.apply(employee1, ['Department head', 500])); // Hanna is Department head and earns $1500
+console.log(promote.apply(employee2, ['Middle developer', 300])); // Bill is Middle developer and earns $1100
