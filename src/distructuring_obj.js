9d1a89992d481a4041fed27f29657448e060cf0b
Viktor Sivulskiy <svicvg@gmail.com>
2018-09-25 17:43:12 +0600

    An distructuring_obj.js File Was Added

---
+++ src/distructuring_obj.js
@@ -0,0 +1,48 @@
+// distructuring object assignment
+
+let user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    social: {
+        facebook: 'johndoe',
+        twitter: 'jdoe'
+    }
+};
+
+let person = {
+    firstName: 'John',
+    lastName: 'Doe'
+};
+
+//let {firstName: first, lastName: last} = person;
+
+//let { firstName: first, lastName: last, age = 25 } = { firstName: 'John', lastName: 'Doe' };
+
+let { firstName: first, lastName: last, social: { facebook: fb }, age = 25 } = user;
+
+console.log(first, last, fb, age);
+
+// function post(url, { data, cache }) {
+//     console.log(data, cache);
+// }
+
+function post(url, { data: { firstName, lastName }, cache }) {
+    console.log(firstName, lastName, cache);
+}
+
+let result = post('api/users', { data: user, cache: false });
+
+function getUserInfo() {
+    return {
+        firstName: 'John',
+        lastName: 'Doe',
+        social: {
+            facebook: 'johndoe',
+            twitter: 'jdoe'
+        }
+    };
+}
+
+let { firstName, lastName, social: { twitter } } = getUserInfo();
+
+console.log(firstName, lastName, twitter);
