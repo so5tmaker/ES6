c88893d63c102cdb08dfdb1c13c93809927b3445
Viktor Sivulskiy <svicvg@gmail.com>
2018-10-17 11:23:02 +0600

    The obj_setters_getters.js File Was Changed

--- dist/8.1 obj_setters_getters.js
+++ dist/8.1 obj_setters_getters.js
@@ -4,12 +4,36 @@ var firstName = 'Bill',
     lastName = 'Gates',
     email = 'billgates@microsoft.com';
 
+// ES5
 var person = {
     firstName: firstName,
     lastName: lastName,
     email: email,
     sayHello: function sayHello() {
-        console.log('Hi, my name is ' + this.firstName + ' ' + this.lastName);
+        console.log('ES5', 'Hi, my name is ' + this.firstName + ' ' + this.lastName);
+    }
+};
+
+Object.defineProperty(person, 'fullName', {
+    get: function get() {
+        return this.firstName + ' ' + this.lastName;
+    },
+    set: function set(value) {
+        this.firstName = value;
+    }
+});
+console.log('\nES5', person);
+console.log('\nES5 getter fullName:', person.fullName + '\n');
+person.fullName = 'Steve';
+console.log('ES5 after setter fullName:', person.fullName + '\n');
+
+// ES6
+var personES6 = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    sayHello: function sayHello() {
+        console.log('ES6', 'Hi, my name is ' + this.firstName + ' ' + this.lastName);
     },
 
     get fullName() {
@@ -20,14 +44,4 @@ var person = {
     }
 };
 
-// ES5
-// Object.defineProperty(person, 'fullName', {
-//     get: function () {
-//         return this.firstName + ' ' + this.lastName;
-//     },
-//     set: function (value) {
-//         this.firstName = value;
-//     }
-// });
-
-console.log(person);
+console.log('\nES6', personES6);
--- src/8.1 obj_setters_getters.js
+++ src/8.1 obj_setters_getters.js
@@ -2,12 +2,36 @@ let firstName = 'Bill',
     lastName = 'Gates',
     email = 'billgates@microsoft.com';
 
+// ES5
 let person = {
     firstName: firstName,
     lastName: lastName,
     email: email,
+    sayHello: function sayHello() {
+        console.log('ES5', `Hi, my name is ${this.firstName} ${this.lastName}`);
+    },
+};
+
+Object.defineProperty(person, 'fullName', {
+    get: function () {
+        return this.firstName + ' ' + this.lastName;
+    },
+    set: function (value) {
+        this.firstName = value;
+    }
+});
+console.log('\nES5', person);
+console.log('\nES5 getter fullName:', person.fullName + '\n');
+person.fullName = 'Steve';
+console.log('ES5 after setter fullName:', person.fullName + '\n');
+
+// ES6
+let personES6 = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
     sayHello() {
-        console.log(`Hi, my name is ${this.firstName} ${this.lastName}`);
+        console.log('ES6', `Hi, my name is ${this.firstName} ${this.lastName}`);
     },
     get fullName() {
         return this.firstName + ' ' + this.lastName;
@@ -17,14 +41,4 @@ let person = {
     }
 };
 
-// ES5
-// Object.defineProperty(person, 'fullName', {
-//     get: function () {
-//         return this.firstName + ' ' + this.lastName;
-//     },
-//     set: function (value) {
-//         this.firstName = value;
-//     }
-// });
-
-console.log(person);
+console.log('\nES6', personES6);
