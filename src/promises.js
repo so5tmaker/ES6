7c776087a35900ecdbbc7ab42065ca3f99fb0e63
Sivulskiy Viktor <svicvg@gmail.com>
2019-01-07 10:42:19 +0600

    Promises were added

---
+++ src/promises.js
@@ -0,0 +1,41 @@
+function applyForVisa(documents) {
+    console.log('Обработка заявления...');
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(function() {
+            Math.random() > 0 ? resolve({}) : reject('В визе отказано: нехватка документов');
+        }, 2000);
+    });
+    return promise;
+}
+
+function getVisa(visa) {
+    console.info('Виза получена');
+    // return visa;
+    return new Promise(function(resolve, reject){
+        // resolve(visa);
+        setTimeout(() => resolve(visa), 2000);
+    });
+}
+
+function bookHotel(visa) {
+    console.log(visa);
+    console.log('Бронируем отель');
+    // return {};
+    // return new Promise(function(resolve, reject, visa){
+    //     // reject('There are no places');
+    //     resolve(visa);
+    // });
+    // return Promise.reject('There are no places');
+    return Promise.resolve(visa);
+}
+
+function buyTickets(booking) {
+    console.log('Покупаем билеты');
+    console.log('Бронь', booking);
+}
+
+applyForVisa({})
+    .then(getVisa)
+    .then(bookHotel)
+    .then(buyTickets)
+    .catch(error => console.error(error));
