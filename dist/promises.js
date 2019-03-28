19ca25c781fadf084f946f09d03d7a47c35c2edd
Sivulskiy Viktor <sivulsky.viktor@vedidev.com>
2019-03-28 15:38:36 +0600

    arrays.js was added

---
+++ dist/promises.js
@@ -0,0 +1,43 @@
+'use strict';
+
+function applyForVisa(documents) {
+    console.log('Обработка заявления...');
+    var promise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            Math.random() > 0 ? resolve({}) : reject('В визе отказано: нехватка документов');
+        }, 2000);
+    });
+    return promise;
+}
+
+function getVisa(visa) {
+    console.info('Виза получена');
+    // return visa;
+    return new Promise(function (resolve, reject) {
+        // resolve(visa);
+        setTimeout(function () {
+            return resolve(visa);
+        }, 2000);
+    });
+}
+
+function bookHotel(visa) {
+    console.log(visa);
+    console.log('Бронируем отель');
+    // return {};
+    // return new Promise(function(resolve, reject, visa){
+    //     // reject('There are no places');
+    //     resolve(visa);
+    // });
+    // return Promise.reject('There are no places');
+    return Promise.resolve(visa);
+}
+
+function buyTickets(booking) {
+    console.log('Покупаем билеты');
+    console.log('Бронь', booking);
+}
+
+applyForVisa({}).then(getVisa).then(bookHotel).then(buyTickets).catch(function (error) {
+    return console.error(error);
+});
---
+++ src/arrays.js
@@ -0,0 +1,35 @@
+var string1 = 'r_liteprofile r_emailaddress w_member_social',
+    string2 = 'r_liteprofile r_emailaddress',
+    string3 = 'r_liteprofile';
+
+// let checkAccess = (cookieArray, linkedinArray) => linkedinArray.every(arrayValue => cookieArray.indexOf(arrayValue) >= 0);
+
+var checkAccess = function (cookieString, permissionsString) {
+    var cookieArray = cookieString.split(" ");
+    var permissionsArray = permissionsString.split(" ");
+    return permissionsArray.every(function (arrayValue) {
+        return cookieArray.indexOf(arrayValue) >= 0;
+    });
+};
+
+// eslint-disable-next-line no-console
+console.log(checkAccess(string2, string1));
+// eslint-disable-next-line no-console
+console.log(checkAccess(string3, string1));
+// eslint-disable-next-line no-console
+console.log(checkAccess(string1, string2));
+// eslint-disable-next-line no-console
+console.log(checkAccess(string2, string3));
+
+var cookieString = "";
+// eslint-disable-next-line no-console
+console.log(cookieString.split(" "));
+
+function hasAccess(permissionsString) {
+    var cookieString = cookieManager.get(LI_ACC_SCOPES) || "";
+    var cookieArray = cookieString.split(" ");
+    var permissionsArray = permissionsString.split(" ");
+    return permissionsArray.every(function (arrayValue) {
+      return cookieArray.indexOf(arrayValue) >= 0;
+    });
+  }
