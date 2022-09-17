1507df5f30fb0d1bac114279e9ee5faa769ddce2
Viktor Sivulskiy <info@softmaker.kz>
2022-09-17 08:31:58 +0600

    setAlarm

--- src/codewars.js
+++ src/codewars.js
@@ -1,6 +1,6 @@
-const sortedSquares = (nums) => nums.map(i =>
-    Math.pow(i, 2)
-).sort((a, b) => a - b > 0 ? 1 : -1);
+const setAlarm = (employed, vacation) => employed && !vacation;
 
-
-console.log(sortedSquares([-7, -3, 2, 3, 11]));
+console.log(setAlarm(true, true));
+console.log(setAlarm(false, true));
+console.log(setAlarm(false, false));
+console.log(setAlarm(true, false));
