e9bf75f9a9e09b7e23c549dbfd2f1f4731ae27cd
Viktor Sivulskiy <info@softmaker.kz>
2022-09-21 10:56:26 +0600

    findOdd

--- src/codewars.js
+++ src/codewars.js
@@ -1,25 +1,27 @@
-const binaryGap = (n) => {
-    const binary = n.toString(2);
-    console.log(binary);
-    let end = binary.length - 1;
-    for (end; end >= 0; end--) {
-        if (binary[end] !== "0") {
-            break;
+function findOddMy(A) {
+    const B = new Map(), C = [];
+    for (let i = 0; i < A.length; i++) {
+        const num = A[i];
+        const maxNew = !C[num] ? 1 : C[num] + 1;
+        C[num] = maxNew;
+        if (maxNew % 2 > 0) {
+            B.set(num, maxNew);
+        } else {
+            B.delete(num);
         }
     }
-    let quantity = 0;
-    let distance = 0;
-    for (let i = end - 1; i >= 0; i--) {
-        const num = binary[i];
-        if (num === "0") {
-            quantity++;
-        }
-        if (num === "1") {
-            distance = Math.max(++quantity, distance);
-            quantity = 0;
-        }
-    }
-    return distance;
-};
+    return Array.from(B.keys())[0];
+}
 
-console.log(binaryGap(647));
+const findOdd1 = (xs) => xs.reduce((a, b) => a ^ b);
+
+function findOdd(arr) {
+    return arr.find((item, index) => arr.filter(el => el == item).length % 2)
+}
+
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]));
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]));
+console.log(findOdd([10]));
+console.log(findOdd([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]));
+console.log(findOdd([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10]));
