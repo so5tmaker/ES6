4803a830baf50b79ea6e70a534e7bf925784b3c8
Viktor Sivulskiy <info@softmaker.kz>
2022-09-27 10:41:09 +0600

    A Narcissistic Number

--- src/codewars.js
+++ src/codewars.js
@@ -1,30 +1,5 @@
-var merge = function (intervals) {
-    let out = [];
-    if (intervals.length < 2) {
-        return intervals;
-    }
-    intervals.sort((a, b) => a[0] - b[0]);
-    for (let i = 0; i < intervals.length; i++) {
-        if (i === 0) {
-            out.push(intervals[i]);
-            continue;
-        }
-        const currentFirst = out.length === 0 ? intervals[i - 1][0] : out[out.length - 1][0];
-        const currentSecond = out.length === 0 ? intervals[i - 1][1] : out[out.length - 1][1];
-        if (currentSecond >= intervals[i][0]) {
-            out.pop();
-            const first = currentFirst > intervals[i][0] ? intervals[i][0] : currentFirst;
-            const second = currentSecond > intervals[i][1] ? currentSecond : intervals[i][1];
-            if (currentFirst > intervals[i][0] && currentFirst > intervals[i][1]) {
-                out.push(intervals[i], intervals[i - 1]);
-                continue;
-            }
-            out.push([first, second]);
-            continue;
-        }
-        out.push(intervals[i]);
-    }
-    return out;
-};
+function narcissistic(value) {
+    return [...String(value)].reduce((p, c) => (p + Math.pow(c, String(value).length)), 0) === value;
+}
 
-console.log(merge([[1, 4], [0, 2], [3, 5]]));
+console.log(narcissistic(153));
