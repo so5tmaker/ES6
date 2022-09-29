fba2c4c7ca6c673464e2b5f2145bc2be833e05bd
Viktor Sivulskiy <info@softmaker.kz>
2022-09-29 15:41:33 +0600

    My search and codewars

--- src/codewars.js
+++ src/codewars.js
@@ -1,29 +1,25 @@
-function search(n, root) {
+function traverseDFRecursive(node, callback) {
+    if (callback(node)) return true;
+
+    if (node.left) {
+        if (traverseDFRecursive(node.left, callback)) return true;
+    }
+
+    if (node.right) {
+        if (traverseDFRecursive(node.right, callback)) return true;
+    }
+
+    return false;
+}
+
+function MySearch(n, root) {
     if (!root) return false;
     if (root.value === n) return true;
-    const find = (root) => {
-        let currentLeft = root && root.left;
-        let currentRight = root && root.right;
-        while (currentLeft || currentRight) {
-            if (currentLeft) {
-                console.log('currentLeft', currentLeft.value);
-                if (currentLeft.value === n) {
-                    return true;
-                };
-                return find(currentLeft.left);
-            }
-            if (currentRight) {
-                console.log('currentRight', currentRight.value);
-                if (currentRight.value === n) {
-                    return true;
-                };
-                return find(currentRight.right);
-            }
-        }
-        return false;
-    }
-    return find(root);
+
+    return traverseDFRecursive(root, node => node.value === n);
 }
+
+const search = (n, root) => !!root && (root.value === n || search(n, root.left) || search(n, root.right));
 
 class Node {
     constructor(value, left = null, right = null) {
@@ -36,7 +32,7 @@ const root = new Node(666, new Node(555), null);
 // console.log(root);
 // console.log(search(555, root));
 
-const SIZE = 5;
+const SIZE = 20;
 const nodes = Array.from({ length: SIZE }, (_, i) => new Node(i));
 
 for (let i = 0; i < SIZE; i++) {
@@ -44,8 +40,6 @@ for (let i = 0; i < SIZE; i++) {
     const idx_right = 2 * i + 2;
     nodes[i].left = nodes[idx_left] || null;
     nodes[i].right = nodes[idx_right] || null;
-    console.log('idx_left', idx_left);
-    console.log('idx_right', idx_right);
 }
 
-console.log(search(2, nodes[0]));
+console.log(search(21, nodes[0]));
