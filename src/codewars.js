5d0a3d9f645433e6e572bc30504371fb2811db69
Viktor Sivulskiy <info@softmaker.kz>
2022-09-29 08:31:03 +0600

    Binary Tree Search (not BST)

--- src/codewars.js
+++ src/codewars.js
@@ -1,19 +1,28 @@
 function search(n, root) {
     if (!root) return false;
     if (root.value === n) return true;
-    let currentLeft = root.left;
-    let currentRight = root.right;
-    while (currentLeft || currentRight) {
-        if (currentLeft) {
-            if (currentLeft.value === n) return true;
-            currentLeft = currentLeft.left;
+    const find = (root) => {
+        let currentLeft = root && root.left;
+        let currentRight = root && root.right;
+        while (currentLeft || currentRight) {
+            if (currentLeft) {
+                console.log('currentLeft', currentLeft.value);
+                if (currentLeft.value === n) {
+                    return true;
+                };
+                return find(currentLeft.left);
+            }
+            if (currentRight) {
+                console.log('currentRight', currentRight.value);
+                if (currentRight.value === n) {
+                    return true;
+                };
+                return find(currentRight.right);
+            }
         }
-        if (currentRight) {
-            if (currentRight.value === n) return true;
-            currentRight = currentRight.left;
-        }
+        return false;
     }
-    return false;
+    return find(root);
 }
 
 class Node {
@@ -24,10 +33,10 @@ class Node {
     }
 }
 const root = new Node(666, new Node(555), null);
-console.log(root);
-console.log(search(555, root));
+// console.log(root);
+// console.log(search(555, root));
 
-const SIZE = 20;
+const SIZE = 5;
 const nodes = Array.from({ length: SIZE }, (_, i) => new Node(i));
 
 for (let i = 0; i < SIZE; i++) {
@@ -35,6 +44,8 @@ for (let i = 0; i < SIZE; i++) {
     const idx_right = 2 * i + 2;
     nodes[i].left = nodes[idx_left] || null;
     nodes[i].right = nodes[idx_right] || null;
+    console.log('idx_left', idx_left);
+    console.log('idx_right', idx_right);
 }
 
-console.log(nodes);
+console.log(search(2, nodes[0]));
