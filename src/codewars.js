dc8dd950581d7ea8869602fa36d35b299eb1bdb1
Viktor Sivulskiy <info@softmaker.kz>
2022-09-30 10:29:58 +0600

    twoSum

--- src/codewars.js
+++ src/codewars.js
@@ -1,5 +1,13 @@
 var twoSum = function (nums, target) {
-
+    const Nums = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let numberToFind = target - nums[i];
+        if (Nums.has(numberToFind)) {
+            return [Nums.get(numberToFind), i];
+        }
+        Nums.set(nums[i], i);
+    }
+    return [];
 };
 
-console.log(twoSum(21, nodes[0]));
+console.log(twoSum([2, 7, 11, 15], 9));
