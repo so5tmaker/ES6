2f245d08e1040cfb702b0b6a8f244e086710c4d0
Viktor Sivulskiy <info@softmaker.kz>
2022-10-10 07:46:15 +0600

    Delete Node in a Linked List

--- src/codewars.js
+++ src/codewars.js
@@ -1,26 +1,5 @@
-var removeDuplicatesMy = function (nums) {
-    let l = 1;
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[l] !== nums[i]) {
-            nums[l] = nums[i];
-            l++;
-        }
-    }
-    return l;
+var deleteNode = function (node) {
+
 };
 
-var removeDuplicates = function (nums) {
-    let insertIndex = 1;
-    for (let i = 1; i < nums.length; i++) {
-        // We skip to next index if we see a duplicate element
-        if (nums[i - 1] != nums[i]) {
-            /* Storing the unique element at insertIndex index and incrementing
-               the insertIndex by 1 */
-            nums[insertIndex] = nums[i];
-            insertIndex++;
-        }
-    }
-    return nums;
-};
-
-console.log(removeDuplicatesMy([1, 1, 2]));
+console.log(deleteNode([4, 5, 1, 9]));
