1b29f10a9ae198901b64b220520f6e44892506b9
Viktor Sivulskiy <info@softmaker.kz>
2022-10-19 10:59:38 +0600

    quickSort grokking

---
+++ grokking/quick_sort.js
@@ -0,0 +1,24 @@
+let arr = [0, 3, 2, 5, 6, 8, 1, 9, 4, 2, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23, 6, 2, 35, 6, 3, 32] // [0,1,1,2,3.......]
+arr = [8, 3, -1, 12, 6, -2, 0] // [0,1,1,2,3.......]
+
+function quickSort(array) {
+    if (array.length <= 1) {
+        return array
+    }
+    let pivotIndex = Math.floor(array.length / 2);
+    let pivot = array[pivotIndex]
+    let less = []
+    let greater = []
+    for (let i = 0; i < array.length; i++) {
+        if (i === pivotIndex)
+            continue
+        if (array[i] < pivot) {
+            less.push(array[i])
+        } else {
+            greater.push(array[i])
+        }
+    }
+    return [...quickSort(less), pivot, ...quickSort(greater)]
+}
+
+console.log(quickSort(arr))
