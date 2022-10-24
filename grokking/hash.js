9137f6bd9579a4e079e0b85365d2cc1deb42232b
Viktor Sivulskiy <info@softmaker.kz>
2022-10-24 09:30:57 +0600

    checkVoter grokking

--- grokking/hash.js
+++ grokking/hash.js
@@ -2,5 +2,25 @@ book = {};
 
 book['apple'] = 0.67;
 book['milk'] = 1.49;
+book['avocado'] = 1.49;
 
 console.log(book);
+
+
+const checkVoter = (name, voted) => {
+    if (name in voted) {
+        console.log("kick them out!");
+    } else {
+        voted[name] = true;
+        console.log("let them vote!");
+    }
+}
+
+let voted = {};
+
+checkVoter("Mike", voted);
+console.log(voted);
+checkVoter("John", voted);
+console.log(voted);
+checkVoter("Mike", voted);
+console.log(voted);
