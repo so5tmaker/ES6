dbaa3757426cbc1c2d63249b9b8caaab3cd7525a
Viktor Sivulskiy <info@softmaker.kz>
2022-11-04 15:07:59 +0600

    promise all qazdevelop

---
+++ interviews/promise_all_qazdevelop.js
@@ -0,0 +1,3 @@
+const promise1 = new Promise(resolve => setTimeout(() => resolve(1), 5000));
+const promise2 = new Promise(resolve => setTimeout(() => resolve(2), 5000));
+const promise3 = new Promise(res)
