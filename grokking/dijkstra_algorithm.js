7726995abf019675582f1ee6e0c3a1c2007b3e7d
Viktor Sivulskiy <info@softmaker.kz>
2022-11-07 11:34:30 +0600

    dijkstra algorithm grokking

--- grokking/dijkstra_algorithm.js
+++ grokking/dijkstra_algorithm.js
@@ -9,15 +9,44 @@ graph.e = { f: 1 }
 graph.f = { g: 1 }
 graph.g = {}
 
-function shortPath(graph, start, end) {
-
-    return costs
+function shortPath(graph, start) {
+    const costs = {};
+    const processed = [];
+    let neighbors = {};
+    Object.keys(graph).forEach((node) => {
+        if (node !== start) {
+            costs[node] = graph[start][node] || 999999999999999;
+        }
+    });
+    let node = findNodeLowestCost(costs, processed);
+    while (node) {
+        const cost = costs[node];
+        neighbors = graph[node];
+        Object.keys(neighbors).forEach(neighbor => {
+            let newCost = cost + neighbors[neighbor];
+            if (newCost < costs[neighbor]) {
+                costs[neighbor] = newCost;
+            }
+        });
+        console.log(costs);
+        processed.push(node);
+        node = findNodeLowestCost(costs, processed);
+    }
+    return costs;
 }
 
 
 function findNodeLowestCost(costs, processed) {
-
-    return lowestNode
+    let lowestCost = 999999999999999;
+    let lowestNode;
+    Object.keys(costs).forEach((node) => {
+        let cost = costs[node];
+        if (cost < lowestCost && !processed.includes(node)) {
+            lowestCost = cost;
+            lowestNode = node;
+        }
+    })
+    return lowestNode;
 }
 
-console.log(shortPath(graph, 'a', 'g'));
+console.log(shortPath(graph, 'a'));
--- interviews/promise_all_qazdevelop.js
+++ interviews/promise_all_qazdevelop.js
@@ -1,3 +1,14 @@
 const promise1 = new Promise(resolve => setTimeout(() => resolve(1), 5000));
 const promise2 = new Promise(resolve => setTimeout(() => resolve(2), 5000));
-const promise3 = new Promise(res)
+const promise3 = new Promise(resolve => setTimeout(() => resolve(3), 5000));
+
+const promises = [promise1, promise2, promise3];
+
+const promiseAll = (all) => {
+    let count = 0;
+    for (i = 1; i > all.length; i++) {
+        all[i]
+            .then()
+    }
+    return false;
+}
