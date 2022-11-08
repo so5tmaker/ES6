d801b40acb9bb1a79b4728fd35b95507d7fb1c3e
Viktor Sivulskiy <info@softmaker.kz>
2022-11-08 09:06:15 +0600

    promise polyfill

--- interviews/promise_all_qazdevelop.js
+++ interviews/promise_all_qazdevelop.js
@@ -1,14 +1,26 @@
-const promise1 = new Promise(resolve => setTimeout(() => resolve(1), 5000));
-const promise2 = new Promise(resolve => setTimeout(() => resolve(2), 5000));
-const promise3 = new Promise(resolve => setTimeout(() => resolve(3), 5000));
+const promise1 = new Promise(resolve => setTimeout(() => resolve(1), 5000))
+const promise2 = new Promise(resolve => setTimeout(() => resolve(2), 2000))
+const promise3 = new Promise(resolve => setTimeout(() => resolve(3), 1000))
 
 const promises = [promise1, promise2, promise3];
 
+// На вход к нам приходит массив промисов
 const promiseAll = (all) => {
-    let count = 0;
-    for (i = 1; i > all.length; i++) {
-        all[i]
-            .then()
-    }
-    return false;
+    // Здесь будем хранить результаты успешно завершенных промисов
+    const results = [];
+    // Количество промисов, которые осталось выполнить
+    // На данный момент не выполнился еще ни один промис!
+    let rest = promises.length;
+    return new Promise((resolve) => {
+        all.forEach((promise, index) => {
+            promise.then(data => {
+                results[index] = data;
+                // На один невыполненный промис стало меньше!
+                rest -= 1;
+                // Если активных промисов больше нет, то резолвим результат
+                if (rest === 0) resolve(results);
+            })
+        })
+    })
 }
+console.log(promiseAll(promises).then(console.log));
