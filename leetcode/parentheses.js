9620d6b1c56670aa6df90c014776aa8122ed1963
Viktor Sivulskiy <info@softmaker.kz>
2023-01-17 08:23:52 +0600

    edit parentheses leetcode

--- leetcode/parentheses.js
+++ leetcode/parentheses.js
@@ -1,13 +1,13 @@
 let c = '(())()';
 let count = 0;
-for (let i = 0; i < c.length; i++) { // console.log(c[i]); 
+for (let i = 0; i < c.length; i++) {
     if (c[i] == '(') {
         count++;
     }
     if (c[i] == ')') {
         count--;
     }
-    if (count < 0) { //console.log(false); 
+    if (count < 0) {
         break;
     }
 }
