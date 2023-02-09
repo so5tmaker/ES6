086d8fec9f4d368b045d75d1382b99604220fa57
Viktor Sivulskiy <info@softmaker.kz>
2023-02-09 08:23:41 +0600

    Sorted Union

--- src/freecodecamp/FearNotLetter.js
+++ src/freecodecamp/FearNotLetter.js
@@ -21,16 +21,119 @@
 
 
 const fearNotLetter = (str) => {
-    alphabet = "abcdefghijklmnopqrstuvwxyz";
-    const newStr = alphabet.substring(alphabet.indexOf(str[0]), str.length + 1);
-    console.log(newStr);
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const newStr = alphabet.substring(alphabet.indexOf(str[0]));
     const letter = newStr.split('').filter((item) => (str.indexOf(item) === -1))
     return letter[0];
 }
 
-// console.log(fearNotLetter("abce"));
-// console.log(fearNotLetter("abcdefghjklmno"));
+console.log(fearNotLetter("abce"));
+console.log(fearNotLetter("abcdefghjklmno"));
 console.log(fearNotLetter("stvwx"));
-// console.log(fearNotLetter("bcdf"));
-// console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
+console.log(fearNotLetter("bcdf"));
+console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
 
+function fearNotLetter1(str) {
+    for (let i = 0; i < str.length; i++) {
+        /* code of current character */
+        const charCode = str.charCodeAt(i);
+        /* if code of current character is not equal to first character + no of iteration
+            then a letter was skipped */
+        if (charCode !== str.charCodeAt(0) + i) {
+            /* if current character skipped past a character find previous character and return */
+            return String.fromCharCode(charCode - 1);
+        }
+    }
+    return undefined;
+}
+
+// test here
+console.log(fearNotLetter1("abce"));
+console.log(fearNotLetter1("abcdefghjklmno"));
+console.log(fearNotLetter1("stvwx"));
+console.log(fearNotLetter1("bcdf"));
+console.log(fearNotLetter1("abcdefghijklmnopqrstuvwxyz"));
+
+// Code Explanation
+
+// This solutions makes use of a for loop.
+// Code of encountered character is stored in code.
+// It is checked if code of current character is the expected one(no characters are skipped) 
+// by using the logic - code of current character = code of first character + number of iterations.
+// If a character is missing, the missing character is found and the final string is returned.
+// undefined is returned if there is no missing character in the string.
+
+// Relevant Links
+// JS For Loops Explained 140
+// String.length
+
+function fearNotLetter2(str) {
+    let currCharCode = str.charCodeAt(0);
+    let missing = undefined;
+
+    str
+        .split("")
+        .forEach(letter => {
+            if (letter.charCodeAt(0) === currCharCode) {
+                currCharCode++;
+            } else {
+                missing = String.fromCharCode(currCharCode);
+            }
+        });
+
+    return missing;
+}
+
+// Code Explanation
+
+// First we define variables to store the character code for the first letter in the string, and to store whatever missing letter we may find.
+// We turn the string to an array in order to forEach through it instead of using for and while loops.
+// As we forEach through our letters’ character codes, we go comparing with the one that should be in that position.
+// If the current letter matches, we move the comparison variable to its next position so we can compare on the next cycle.
+// If not, the missing letter will be assigned to the missing variable, which will be returned after the map is finished.
+// If there are no missing characters, return undefined.
+
+// Relevant Links
+// JS String Prototype Split 
+
+function fearNotLetter3(str) {
+    for (let i = 1; i < str.length; ++i) {
+        if (str.charCodeAt(i) - str.charCodeAt(i - 1) > 1) {
+            return String.fromCharCode(str.charCodeAt(i - 1) + 1);
+        }
+    }
+}
+// Code Explanation
+
+// Loop over the string
+// Check if the difference in char codes between adjacent characters in the string is more than 1(check ASCII table)
+// Return the missing character(+1 from where the gap was detected)
+
+function fearNotLetter4(str) {
+    var allChars = "";
+    var notChars = new RegExp("[^" + str + "]", "g");
+
+    for (var i = 0; allChars[allChars.length - 1] !== str[str.length - 1]; i++)
+        allChars += String.fromCharCode(str[0].charCodeAt(0) + i);
+
+    return allChars.match(notChars)
+        ? allChars.match(notChars).join("")
+        : undefined;
+}
+
+// test here
+console.log(fearNotLetter4("abce"));
+
+// Code Explanation
+
+// A new string allChars is created.
+// Create a regular expression notChars which selects everything except str.
+// The for loop is used to add all the letters in the range to allChars.
+//     match() is used to strip off the str letters from the newly created string and it is returned.
+// If there are no missing characters, return undefined.
+
+// Relevant Links
+// JS Regex Resources
+// JS Ternary 41
+// JS String Prototype Match 32
+// JS Array Prototype Join 
---
+++ src/freecodecamp/SortedUnion.js
@@ -0,0 +1,128 @@
+// Sorted Union
+// Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+// In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+// Check the assertion tests for examples.
+
+// uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+// uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+// uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
+// uniteUnique([1, 3, 2], [5, 4], [5, 6]) should return [1, 3, 2, 5, 4, 6].
+// uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+
+const uniteUnique = (arr, ...rest) => {
+    let newArray = [...new Set(arr)];
+    const getUnique = (array, newArray) => {
+        const newUnique = [];
+        for (let i = 0; i < array.length; i++) {
+            if (!newArray.includes(array[i])) { newUnique.push(array[i]) }
+        }
+        return newUnique;
+    }
+    for (let i = 0; i < rest.length; i++) {
+        const unique = getUnique(rest[i], newArray);
+        newArray = [...newArray, ...unique];
+    }
+    return newArray;
+}
+
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])); // should return [1, 3, 2, 5, 4].
+console.log(uniteUnique([1, 2, 3], [5, 2, 1])); // should return [1, 2, 3, 5].
+console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])); // should return [1, 2, 3, 5, 4, 6, 7, 8].
+console.log(uniteUnique([1, 3, 2], [5, 4], [5, 6])); // should return [1, 3, 2, 5, 4, 6].
+console.log(uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1])); // should return [1, 3, 2, 5, 4].
+
+// Problem Explanation
+// The program has to return a new array of unique values from two original arrays in the order they show up.So there is not sorting required, and there shouldn’t be any duplicates.
+
+// Relevant Links
+
+// JS Arguments 3.5k
+// Hints
+// Hint 1
+// Since you have no idea how many parameters were passed, it would be best to loop through the arguments before looping through the arrays.
+
+//     Hint 2
+// It isn’t necessary to use loops.You can use functions such as map(), reduce() or others if you want.
+
+//     Hint 3
+// You will have to check if the current value is already on the array to be returned for every value.
+
+//     Solutions
+// Solution 1(Click to Show / Hide)
+function uniteUnique(arr1, arr2, arr3) {
+    // Creates an empty array to store our final result.
+    const finalArray = [];
+
+    // Loop through the arguments object to truly make the program work with two or more arrays
+    // instead of 3.
+    for (let i = 0; i < arguments.length; i++) {
+        const arrayArguments = arguments[i];
+
+        // Loops through the array at hand
+        for (let j = 0; j < arrayArguments.length; j++) {
+            let indexValue = arrayArguments[j];
+
+            // Checks if the value is already on the final array.
+            if (finalArray.indexOf(indexValue) < 0) {
+                finalArray.push(indexValue);
+            }
+        }
+    }
+
+    return finalArray;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+// Code Explanation
+
+// Create empty array finalResult to store the final result.
+// Loop through the arguments object in the outer loop and store it in arrayArguments.
+// The inner loop is used to loop through individual array elements.
+// If the element doesn’t already exist in finalArray, add it.
+// Return finalArray.
+// Relevant Links
+
+// JS For Loops Explained 90
+// array.length 29
+// JS String Prototype IndexOf 153
+// JS Array Prototype Push 61
+// Solution 2(Click to Show / Hide)
+function uniteUnique(arr) {
+    const args = [...arguments];
+    const result = [];
+    for (let i = 0; i < args.length; i++) {
+        for (let j = 0; j < args[i].length; j++) {
+            if (!result.includes(args[i][j])) {
+                result.push(args[i][j]);
+            }
+        }
+    }
+    return result;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+// Solution 3(Click to Show / Hide)
+function uniteUnique(...arr) {
+    return [...new Set(arr.flat())];
+}
+
+// Or as an arrow function
+const uniteUnique = (...arr) => [...new Set(arr.flat())];
+// Relevant Links
+
+// Array.prototype.flat 330
+// Arguments 262
+// Set 1.5k
+// Solution 4(Click to Show / Hide)
+function uniteUnique() {
+    return [...arguments]
+        .flat()
+        .filter((item, ind, arr) => arr.indexOf(item) === ind);
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
