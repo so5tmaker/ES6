5853ff11ce354e6e7deecec45e44a64e8ad89954
Viktor Sivulskiy <info@softmaker.kz>
2023-04-06 07:41:07 +0600

    print num two

--- freecodecamp/MapDebris.js
+++ freecodecamp/MapDebris.js
@@ -21,3 +21,15 @@ const orbitalPeriod = (arr) => {
 console.log(orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }])); // should return [{ name: "sputnik", orbitalPeriod: 86400 }].
 console.log(orbitalPeriod([{ name: "iss", avgAlt: 413.6 }, { name: "hubble", avgAlt: 556.7 }, { name: "moon", avgAlt: 378632.553 }]));
 // should return [{ name: "iss", orbitalPeriod: 5557 }, { name: "hubble", orbitalPeriod: 5734 }, { name: "moon", orbitalPeriod: 2377399 }].
+
+var printNumTwo;
+let hj = 'jgjgj';
+hj.to
+for (var i = 0; i < 2; i++) {
+    if (i === 1) {
+        printNumTwo = function () {
+            return i;
+        };
+    }
+}
+console.log(printNumTwo());
