574147d86d055809f00675ae3ecfcd122bdf48b1
Viktor Sivulskiy <info@softmaker.kz>
2023-05-22 07:50:18 +0600

    insertionSort

--- freecodecamp/IntermediateAlgorithmScripting/InsertionSort.js
+++ freecodecamp/IntermediateAlgorithmScripting/InsertionSort.js
@@ -9,9 +9,23 @@
 
 // Instructions: Write a function insertionSort which takes an array of integers as input and 
 // returns an array of these integers in sorted order from least to greatest.
+const swap = (array, j, k) => {
+    const tmp = array[j];
+    array[j] = array[k];
+    array[k] = tmp;
+}
 
 const insertionSort = (array) => {
-    // Only change code below this line
+    for (let i = 0; i < array.length; i++) {
+        let j = i;
+        while (j > 0 && array[j] < array[j - 1]) {
+            swap(array, j - 1, j);
+            j--;
+        }
+    }
     return array;
-    // Only change code above this line
 }
+
+
+console.log(insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
+console.log(insertionSort([5, 4, 33, 2, 8]));
