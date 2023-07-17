07bc365589c051b496e12438ee7e8fff0d55e5b1
Viktor Sivulskiy <info@softmaker.kz>
2023-07-17 09:07:50 +0600

    80. Remove Duplicates from Sorted Array II 2nd v

--- leetcode/80.remove-duplicates-from-sorted-array-2.js
+++ leetcode/80.remove-duplicates-from-sorted-array-2.js
@@ -1,21 +1,39 @@
 // 80. Remove Duplicates from Sorted Array II
 
 var removeDuplicates = function (nums) {
-    let j = 1;
-    let k = 0;
     let l = 0;
-    for (i = 0; i < nums.length; i++) {
-        if (j <= 2) k++;
-        if (j > 2) { l = i; j = 1; }
-        if (nums[i] === nums[i + 1]) {
-            j++;
-        } else {
-            nums[l] = i < (nums.length - 1) ? nums[i + 1] : nums[i];
-            l = i;
+    let r = 0;
+
+    while (r < nums.length) {
+        let count = 1;
+        while (r + 1 < nums.length && nums[r] === nums[r + 1]) {
+            r++;
+            count++;
         }
+        for (i = 0; i < Math.min(2, count); i++) {
+            nums[l] = nums[r];
+            l++;
+        }
+        r++;
     }
-    return [nums, k];
+    return l;
 };
 
+var removeDuplicates2 = function (nums) {
+    let j = 1, count = 1;
+
+    for (i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) {
+            count++;
+        } else {
+            count = 1;
+        }
+        if (count <= 2) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    return j;
+}
 console.log(removeDuplicates([1, 1, 1, 2, 2, 3]));
 console.log(removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]));
