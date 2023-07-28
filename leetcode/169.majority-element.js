e39785603fb1a8742b7be748d674b5a480137baa
Viktor Sivulskiy <info@softmaker.kz>
2023-07-28 08:02:45 +0600

    189.rotate-array

--- leetcode/169.majority-element.js
+++ leetcode/169.majority-element.js
@@ -9,26 +9,40 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function (nums) {
-    let majorityElement = nums[0];
+function majorityElement(nums) {
+    let majority = nums[0];
     let count = 1;
 
     for (let i = 1; i < nums.length; i++) {
-        if (nums[i] === majorityElement) {
+        if (nums[i] === majority) {
             count++;
         } else {
             count--;
         }
 
         if (count === 0) {
-            majorityElement = nums[i];
+            majority = nums[i];
             count = 1;
         }
     }
 
-    return majorityElement;
-};
-const nums = [3, 3, 4, 2, 4, 4, 2, 4, 4];
-console.log(majorityElement(nums)); // Output: 4
+    // Verify if the majority is a majority element
+    count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === majority) {
+            count++;
+        }
+    }
+
+    if (count > nums.length / 2) {
+        return majority;
+    } else {
+        return null; // No majority element found
+    }
+}
+
+console.log(majorityElement([3, 2, 3])); // Output: 3
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // Output: 2
+console.log(majorityElement([1, 1, 1, 2, 2, 2])); // Output: 2
 // @lc code=end
 
