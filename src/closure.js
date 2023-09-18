379a312b6e2574c198f1d63ba8d6906ab92a4b68
Viktor Sivulskiy <info@softmaker.kz>
2023-09-18 09:05:13 +0600

    closure

--- src/closure.js
+++ src/closure.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const a = 1;
 
 function name(a) {
@@ -13,3 +15,28 @@ console.log(a);
 // 1
 // 2
 // 1
+
+
+
+
+const User = function () {
+    this.name = 'Alex';
+    console.log(this);
+    return {
+        name: 'Sasha',
+        getName() {
+            console.log(this, this.name);
+            return this.name;
+        },
+        getArrowName: () => {
+            console.log(this, this.name);
+            return this.name;
+        }
+    };
+}
+
+const u = new User();
+
+console.log(a);
+u.getName();
+u.getArrowName();
