90e8090f17008f3c273506ef9410ded7324d7304
Viktor Sivulskiy <info@softmaker.kz>
2023-09-28 07:11:53 +0600

    58. Length of Last Word

---
+++ leetcode/58.length-of-last-word.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=58 lang=javascript
+ *
+ * [58] Length of Last Word
+ *
+ * https://leetcode.com/problems/length-of-last-word/description/
+ *
+ * algorithms
+ * Easy (39.69%)
+ * Likes:    4097
+ * Dislikes: 202
+ * Total Accepted:    1.4M
+ * Total Submissions: 3M
+ * Testcase Example:  '"Hello World"'
+ *
+ * Given a string s consisting of words and spaces, return the length of the
+ * last word in the string.
+ * 
+ * A word is a maximal substring consisting of non-space characters only.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "Hello World"
+ * Output: 5
+ * Explanation: The last word is "World" with length 5.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "   fly me   to   the moon  "
+ * Output: 4
+ * Explanation: The last word is "moon" with length 4.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = "luffy is still joyboy"
+ * Output: 6
+ * Explanation: The last word is "joyboy" with length 6.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 10^4
+ * s consists of only English letters and spaces ' '.
+ * There will be at least one word in s.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    
+};
+// @lc code=end
+
