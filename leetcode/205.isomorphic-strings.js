ea7538d64af9b0b53eb89979d97298ca9823f429
Viktor Sivulskiy <info@softmaker.kz>
2023-10-11 08:11:57 +0600

    205 Isomorphic Strings

---
+++ leetcode/205.isomorphic-strings.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=205 lang=javascript
+ *
+ * [205] Isomorphic Strings
+ *
+ * https://leetcode.com/problems/isomorphic-strings/description/
+ *
+ * algorithms
+ * Easy (42.47%)
+ * Likes:    7721
+ * Dislikes: 1670
+ * Total Accepted:    1M
+ * Total Submissions: 2.3M
+ * Testcase Example:  '"egg"\n"add"'
+ *
+ * Given two strings s and t, determine if they are isomorphic.
+ * 
+ * Two strings s and t are isomorphic if the characters in s can be replaced to
+ * get t.
+ * 
+ * All occurrences of a character must be replaced with another character while
+ * preserving the order of characters. No two characters may map to the same
+ * character, but a character may map to itself.
+ * 
+ * 
+ * Example 1:
+ * Input: s = "egg", t = "add"
+ * Output: true
+ * Example 2:
+ * Input: s = "foo", t = "bar"
+ * Output: false
+ * Example 3:
+ * Input: s = "paper", t = "title"
+ * Output: true
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 5 * 10^4
+ * t.length == s.length
+ * s and t consist of any valid ascii character.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    
+};
+// @lc code=end
+
