70c278d3b898e9a1ccd4afeeba246b8b7f27c10f
Viktor Sivulskiy <info@softmaker.kz>
2023-10-13 09:38:49 +0600

    290 Word Pattern

---
+++ leetcode/290.word-pattern.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=290 lang=javascript
+ *
+ * [290] Word Pattern
+ *
+ * https://leetcode.com/problems/word-pattern/description/
+ *
+ * algorithms
+ * Easy (40.34%)
+ * Likes:    6804
+ * Dislikes: 866
+ * Total Accepted:    594.1K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '"abba"\n"dog cat cat dog"'
+ *
+ * Given a pattern and a string s, find if s follows the same pattern.
+ * 
+ * Here follow means a full match, such that there is a bijection between a
+ * letter in pattern and a non-empty word in s.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: pattern = "abba", s = "dog cat cat dog"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: pattern = "abba", s = "dog cat cat fish"
+ * Output: false
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: pattern = "aaaa", s = "dog cat cat dog"
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= pattern.length <= 300
+ * pattern contains only lower-case English letters.
+ * 1 <= s.length <= 3000
+ * s contains only lowercase English letters and spaces ' '.
+ * s does not contain any leading or trailing spaces.
+ * All the words in s are separated by a single space.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    
+};
+// @lc code=end
+
