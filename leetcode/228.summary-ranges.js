c9171903d0becb29da90a7bd963cd6bd2d5e6a85
Viktor Sivulskiy <info@softmaker.kz>
2023-11-02 07:07:05 +0600

    228 Summary Ranges Done

--- leetcode/228.summary-ranges.js
+++ leetcode/228.summary-ranges.js
@@ -76,22 +76,62 @@ var summaryRanges = function (nums) {
     const newNums = [];
     j = 0;
 
+    if (n === 1) newNums.push(String(nums[0]));
+
+    const pushNums = (num1, num2) => {
+        return (num1 < 0) ? [num2, num1] : [num1, num2];
+    }
+
     for (let i = 1; i < n; i++) {
         const current = nums[i - 1];
 
         if (current + 1 < nums[i]) {
-            newNums.push(String(nums[j]) + (j === i - 1 ? '' : '->' + String(current)));
+            const newPushNums = pushNums(nums[j], current);
+            newNums.push(String(newPushNums[0]) + (j === i - 1 ? '' : '->' + String(newPushNums[1])));
             j = i === n - 1 ? j : i;
         }
 
         if (i === n - 1 && j !== i) {
-            newNums.push(String(nums[i]));
+            const newPushNums = pushNums(nums[j], nums[i]);
+            newNums.push(((current + 1 === nums[i]) ? newPushNums[0] + '->' + String(newPushNums[1]) : '' + nums[i]));
         }
     }
 
     return newNums;
 };
-console.log(summaryRanges([0, 1, 2, 4, 5, 7]));
-console.log(summaryRanges([0, 2, 3, 4, 6, 8, 9]));
+
+// console.log(summaryRanges([0, 1, 2, 4, 5, 7]));
+// console.log(summaryRanges([0, 2, 3, 4, 6, 8, 9]));
+// console.log(summaryRanges([0, 1, 2]));
+// console.log(summaryRanges([-2147483648, -2147483647, 2147483647]));
+// console.log(summaryRanges([0, 1, 2, 4, 5, 7, 9, 10, 12, 14, 15, 16, 27, 28]));
+
+function findRanges(nums) {
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let start = nums[i];
+        let end = nums[i];
+
+        while (i + 1 < nums.length && nums[i + 1] - nums[i] === 1) {
+            i++;
+            end = nums[i];
+        }
+
+        if (start === end) {
+            result.push(`${start}`);
+        } else {
+            result.push(`${start}->${end}`);
+        }
+    }
+
+    return result;
+}
+
+console.log(findRanges([0, 1, 2, 4, 5, 7])); // Output: ["0->2", "4->5", "7"]
+console.log(findRanges([0, 2, 3, 4, 6, 8, 9])); // Output: ["0", "2->4", "6", "8->9"]
+console.log(summaryRanges([0, 1, 2]));
+console.log(findRanges([-2147483648, -2147483647, 2147483647]));
+console.log(findRanges([0, 1, 2, 4, 5, 7, 9, 10, 12, 14, 15, 16, 27, 28]));
 // @lc code=end
 
