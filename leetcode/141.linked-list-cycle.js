214ff6d33d667c5aa151b0726eb2ca0d30f08c52
Viktor Sivulskiy <info@softmaker.kz>
2023-11-15 07:52:07 +0600

    141 Linked List Cycle

--- leetcode/141.linked-list-cycle.js
+++ leetcode/141.linked-list-cycle.js
@@ -78,8 +78,18 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {
-    
+var hasCycle = function (head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if (fast === slow) return true;
+    }
+
+    return false;
 };
 // @lc code=end
 
