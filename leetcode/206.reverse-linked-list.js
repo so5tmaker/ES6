6b402331b83ea3c6860af633c11da62239651490
Viktor Sivulskiy <info@softmaker.kz>
2023-11-27 08:02:24 +0600

    206 Reverse Linked List

---
+++ leetcode/206.reverse-linked-list.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (71.95%)
+ * Likes:    20110
+ * Dislikes: 370
+ * Total Accepted:    3.6M
+ * Total Submissions: 4.8M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given the head of a singly linked list, reverse the list, and return the
+ * reversed list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2]
+ * Output: [2,1]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = []
+ * Output: []
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is the range [0, 5000].
+ * -5000 <= Node.val <= 5000
+ * 
+ * 
+ * 
+ * Follow up: A linked list can be reversed either iteratively or recursively.
+ * Could you implement both?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+
+};
+
+// https://youtu.be/hMnwCgnfND4 - Reverse Linked List Solution Explained with Introduction to Linked Lists - Javascript
+// @lc code=end
+
--- leetcode/21.merge-two-sorted-lists.js
+++ leetcode/21.merge-two-sorted-lists.js
@@ -82,5 +82,26 @@ var mergeTwoListsRecursion = function (list1, list2) {
         return list2
     }
 };
+
+
+// initialize a dummy head node
+    
+// initialize a crtNode variable to keep track of the current node, starting with the dummy head node
+    
+// while there are still nodes to compare in two lists
+    // if value of 2nd node is less than value of 1st node
+        // set the current node's link to l2 node
+        // set the l2 node to l2's next node
+    // else
+        // set the current node's link to l1 node
+        // set the l1 node to l1's next node
+        
+    // move on to the next node
+            
+// if one of the lists no longer have any nodes to compare, point crt's link to the remaining nodes in the other list
+// if both lists are empty, point crt's link to null
+
+// return merged linked list
+
 // @lc code=end
 
