a9c2b76ed232e04e3eed377e0154aa98603583e0
Viktor Sivulskiy <info@softmaker.kz>
2024-01-09 07:10:28 +0600

    book hotel promises

--- src/promises.js
+++ src/promises.js
@@ -1,7 +1,7 @@
 function applyForVisa(documents) {
     console.log('Обработка заявления...');
     let promise = new Promise((resolve, reject) => {
-        setTimeout(function() {
+        setTimeout(function () {
             Math.random() > 0 ? resolve({}) : reject('В визе отказано: нехватка документов');
         }, 2000);
     });
@@ -11,7 +11,7 @@ function applyForVisa(documents) {
 function getVisa(visa) {
     console.info('Виза получена');
     // return visa;
-    return new Promise(function(resolve, reject){
+    return new Promise(function (resolve, reject) {
         // resolve(visa);
         setTimeout(() => resolve(visa), 2000);
     });
@@ -21,12 +21,12 @@ function bookHotel(visa) {
     console.log(visa);
     console.log('Бронируем отель');
     // return {};
-    // return new Promise(function(resolve, reject, visa){
-    //     // reject('There are no places');
-    //     resolve(visa);
-    // });
+    return new Promise(function (resolve, reject, visa) {
+        // reject('There are no places');
+        resolve(visa);
+    });
     // return Promise.reject('There are no places');
-    return Promise.resolve(visa);
+    // return Promise.resolve(visa);
 }
 
 function buyTickets(booking) {
