b959dfbe4e596beec7b1d68b54311a31cc97e4eb
Viktor Sivulskiy <info@softmaker.kz>
2024-01-16 07:05:24 +0600

    prototype inheritance

--- src/PrototypeInheritance.js
+++ src/PrototypeInheritance.js
@@ -6,15 +6,26 @@ function Cat(catName) {
     };
 }
 
+function Dog(nickName) {
+    var name = nickName;
+
+    this.getNickName = () => {
+        return name;
+    };
+}
+
 function BobCat(catName) {
     this.weigth = 30;
     this.color = 'orange';
 }
 
 var cat = new Cat('David');
+var dog = new Dog('Bob');
 
-BobCat.prototype = cat;
+// BobCat.prototype = cat;
+BobCat.prototype = dog;
 
 var david = new BobCat();
 
-console.log(david.getName());
+// console.log(david.getName());
+console.log(david.getNickName());
