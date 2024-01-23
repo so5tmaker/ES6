c0331e8ab146579bf70e53ee1bbf7086f546ab5f
Viktor Sivulskiy <info@softmaker.kz>
2024-01-23 13:34:14 +0600

    apply function

--- src/apply.js
+++ src/apply.js
@@ -10,12 +10,19 @@ let employee2 = {
     salary: 800
 }
 
-function promote (newPosition, salaryRise){
-    this.position=newPosition;
-    this.salary+=salaryRise;
+let employee3 = {
+    name: 'John',
+    position: 'Senior developer',
+    salary: 1800
+}
+
+function promote(newPosition, salaryRise) {
+    this.position = newPosition;
+    this.salary += salaryRise;
 
     return `${this.name} is ${this.position} and earns $${this.salary}`;
 }
 
 console.log(promote.apply(employee1, ['Department head', 500])); // Hanna is Department head and earns $1500
 console.log(promote.apply(employee2, ['Middle developer', 300])); // Bill is Middle developer and earns $1100
+console.log(promote.apply(employee3, ['Lead developer', 1300])); // John is Senior developer and earns $3100
