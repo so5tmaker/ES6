3c4fae86a170c50755e0102158c80b88dcaf424f
Viktor Sivulskiy <info@softmaker.kz>
2024-01-24 07:25:24 +0600

    fibonacci_

--- common/fibonacci.js
+++ common/fibonacci.js
@@ -40,8 +40,8 @@ console.log(sequence0); // outputs 55
 
 function fibonacci_(length) {
     let sequence = [0, 1];
-    for (let i = 2; i < length; i++) {
-        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    for (let i = 1; i < length - 1; i++) {
+        sequence[i + 1] = sequence[i] + sequence[i - 1];
     }
     return sequence;
 }
