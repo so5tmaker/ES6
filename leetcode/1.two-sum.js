a06da7beb9d3aaef3285cb3c53fdf0e4939bbd66
Viktor Sivulskiy <info@softmaker.kz>
2024-01-29 07:28:41 +0600

    two sum

--- leetcode/1.two-sum.js
+++ leetcode/1.two-sum.js
@@ -84,5 +84,6 @@ var twoSum = function (nums, target) {
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
+console.log(twoSum([3, 5, 8, 13, 7, 12, 3], 6));
 // @lc code=end
 
