729ee0c00460d313a1a810ffa47d110908217581
Viktor Sivulskiy <info@softmaker.kz>
2024-02-15 07:52:38 +0600

    Merge Two Sorted Lists Recursion

--- leetcode/21.merge-two-sorted-lists.js
+++ leetcode/21.merge-two-sorted-lists.js
@@ -153,18 +153,18 @@ console.log(mergeTwoLists(list1, list2));
 console.log('*******************');
 
 // https://leetcode.com/problems/merge-two-sorted-lists/solutions/2705782/js-recursion-with-exlanation/?envType=study-plan-v2&envId=top-interview-150
-// var mergeTwoListsRecursion = function (list1, list2) {
-//     if (!list1) return list2;
-//     else if (!list2) return list1;
-//     else if (list1.val <= list2.val) {
-//         list1.next = mergeTwoLists(list1.next, list2);
-//         return list1;
-//     } else {
-//         list2.next = mergeTwoLists(list1, list2.next);
-//         return list2
-//     }
-// };
-// console.log(mergeTwoListsRecursion(list1, list2));
-// console.log('*******************');
+var mergeTwoListsRecursion = function (list1, list2) {
+    if (!list1) return list2;
+    else if (!list2) return list1;
+    else if (list1.val <= list2.val) {
+        list1.next = mergeTwoListsRecursion(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoListsRecursion(list1, list2.next);
+        return list2
+    }
+};
+console.log(mergeTwoListsRecursion(list1, list2));
+console.log('*******************');
 
 // @lc code=end
