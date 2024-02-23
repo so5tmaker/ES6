4d0a1547aff6164be260c5b88596953b1c0a6047
Viktor Sivulskiy <info@softmaker.kz>
2024-02-23 07:33:57 +0600

    Linked List Youtube append

---
+++ algs_and_structures/12_linked_list_youtube.js
@@ -0,0 +1,70 @@
+// Связный список (Linked List). Структуры данных | Реализация на JS
+// https://youtu.be/QdCyTTid9-U?t=940
+
+class LinkedListNode {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+
+    toString() {
+        return `${this.value}`;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    append(value) {
+        const newNode = new LinkedListNode(value);
+
+        if (!this.head || !this.tail) {
+            this.head = newNode;
+            this.tail = newNode;
+
+            return this;
+        }
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+
+        return this;
+    }
+
+    size() {
+        let count = 0;
+        let node = this.head;
+        while (node) {
+            count++;
+            node = node.next
+        }
+        return count;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    getLast() {
+        let lastNode = this.head;
+        if (lastNode) {
+            while (lastNode.next) {
+                lastNode = lastNode.next
+            }
+        }
+        return lastNode
+    }
+
+    getFirst() {
+        return this.head;
+    }
+}
+
+const list = new LinkedList();
+
+list.append('a').append('b').append('c');
+console.log('list', JSON.stringify(list));
+console.log(list.size());
