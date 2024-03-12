4a5b8d8f14fe3b69659ebbe23a91c33d99b990d2
Viktor Sivulskiy <info@softmaker.kz>
2024-03-12 07:13:23 +0500

    two pointers divided

--- leetcode/1.two-pointers-yandex.js
+++ leetcode/1.two-pointers-yandex.js
@@ -14,5 +14,27 @@ function maxConsecutiveElements(inputStr) {
 
 console.log(maxConsecutiveElements('success'));
 
+function get_next_dif_letter_idx(input_str, cur_letter_idx) {
+    let next_letter_idx = cur_letter_idx;
+    while (next_letter_idx < input_str.length &&
+        input_str[next_letter_idx] === input_str[cur_letter_idx]) {
+        next_letter_idx++;
+    }
+    return next_letter_idx;
+}
+
+function max_consecutive_elements(input_str) {
+    let result = 0;
+    let cur_letter_idx = 0;
+    while (cur_letter_idx < input_str.length) {
+        let next_letter_idx = get_next_dif_letter_idx(input_str, cur_letter_idx);
+        result = Math.max(result, next_letter_idx - cur_letter_idx);
+        cur_letter_idx = next_letter_idx;
+    }
+    return result;
+}
+
+
+
 
 
