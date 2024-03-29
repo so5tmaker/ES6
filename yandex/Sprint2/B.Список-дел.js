97dc90a6b9263d4e16f396808820b6f7014d182b
Viktor Sivulskiy <info@softmaker.kz>
2024-03-29 07:48:33 +0500

    sprint 2 b to do list

---
+++ yandex/Sprint2/B.Список-дел.js
@@ -0,0 +1,35 @@
+// if (process.env.REMOTE_JUDGE !== 'true') {
+class Node {
+    constructor(value = null, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+// }
+
+function solution(node) {
+    let current = node;
+
+    while (current) {
+        console.log(current.value);
+
+        current = current.next;
+    }
+}
+
+function test() {
+    var node3 = new Node("node3");
+    var node2 = new Node("node2", node3);
+    var node1 = new Node("node1", node2);
+    var node0 = new Node("node0", node1);
+    solution(node0);
+    /*
+    Output is:
+    node0
+    node1
+    node2
+    node3
+    */
+}
+
+test();
