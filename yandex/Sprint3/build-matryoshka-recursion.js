8e7af3fad628339882b078f16516c994a4d875dd
Viktor Sivulskiy <info@softmaker.kz>
2024-04-11 09:51:22 +0500

    sprint 3 as binary string recursion

--- yandex/Sprint3/build-matryoshka-recursion.js
+++ yandex/Sprint3/build-matryoshka-recursion.js
@@ -12,3 +12,13 @@ function buildMatryoshka(size, n) {
 }
 
 buildMatryoshka(4, 3)
+
+function as_binary_string(n) {
+    if (n < 0) {
+        return "-" + as_binary_string(-n);
+    }
+    let last_digit = n % 2;
+    return as_binary_string(Math.floor(n / 2)) + last_digit.toString();
+}
+
+as_binary_string(12);
