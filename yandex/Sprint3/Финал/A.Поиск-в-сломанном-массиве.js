e4043298e68afb0c3845dec6da70029c0c34a6f1
Viktor Sivulskiy <info@softmaker.kz>
2024-04-22 16:37:52 +0500

    sprint 3 final a broken search

--- yandex/Sprint3/Финал/A.Поиск-в-сломанном-массиве.js
+++ yandex/Sprint3/Финал/A.Поиск-в-сломанном-массиве.js
@@ -1,19 +1,18 @@
 /*
-https://contest.yandex.ru/contest/22781/run-report/111734002/
+https://contest.yandex.ru/contest/23815/run-report/112686274/
 
 -- ПРИНЦИП РАБОТЫ --
-
+Я реализовал поиск по сломанному массиву с помощью модифицированного бинарного поиска.
 
 -- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
-
+Использование бинарного поиска позволяет разбивать массив на два и затем эффективно находить искомый элемент, 
+обеспечивая при этом постоянное время выполнения (O(log n)).
 
 -- ВРЕМЕННАЯ СЛОЖНОСТЬ --
-
+Поиск с помощью бинарного поиска выполняется за время O(log n).
 
 -- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
-
-
-Поэтому и моя очередь будет потреблять O(n) памяти.
+Мы не используем дополнительную память, а только переменные, а значит пространственная сложность составляет O(1).
 */
 
 const _readline = require('readline');
@@ -38,19 +37,32 @@ function readNumber() {
     return Number(_inputLines[_curLine++]);
 }
 
-function readNumberArray(size) {
-    const commands = [];
-
-    for (let k = 0; k < size; k++) {
-        commands.push(_inputLines[_curLine++].split(' ').map(i => Number(i)));
-    }
-
-    return commands;
-}
+function readNumberArray() { return _inputLines[_curLine++].split(' ').map(i => Number(i)); }
 
 function brokenSearch(arr, k) {
-    // Your code
-    // “ヽ(´▽｀)ノ”
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((right + left) / 2);
+
+        if (k === arr[mid]) return mid;
+
+        // если левая половина отсортирована
+        if (arr[left] <= arr[mid]) {
+            // если элемент находится в отсортированной левой половине
+            if (arr[left] <= k && k < arr[mid]) { right = mid - 1; }
+            else { left = mid + 1; }
+        } else // если правая половина отсортирована
+        {
+            // если элемент находится в отсортированной правой половине
+            if (k <= arr[right] && k > arr[mid]) { left = mid + 1; }
+            else { right = mid - 1; }
+        }
+
+    }
+
+    return -1;
 }
 
 function solve() {
@@ -61,6 +73,6 @@ function solve() {
     const found = brokenSearch(numbers, item);
 
     if (found > -1) {
-        console.log(found, numbers[found]);
+        console.log(found);
     } else { console.error("WA"); }
 }
