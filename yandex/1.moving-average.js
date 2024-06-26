cec3d38faf52d4df169a8c0804b533142d838ed3
Viktor Sivulskiy <info@softmaker.kz>
2024-06-26 08:29:16 +0500

    intro sliding window

--- yandex/1.moving-average.js
+++ yandex/1.moving-average.js
@@ -1,11 +1,16 @@
-function movingAverage(timeseries, K) {
+function movingAverage(timeSeries, K) {
     let result = []; // Empty array.
-    for (let beginIndex = 0; beginIndex <= timeseries.length - K; beginIndex++) {
+    // внешний выполняет ровно N − K + 1 итераций;
+    for (let beginIndex = 0; beginIndex <= timeSeries.length - K; beginIndex++) {
         let endIndex = beginIndex + K;
         // Iterate over a window of width K.
         let currentSum = 0;
+        /* внутренний выполняет K итераций на каждой из N − K + 1 итераций внешнего цикла N × (N − K + 1) = N × K − K^2 + K.
+        Поскольку в нашем случае N сильно больше K, частью ( −K^2 + K ) в этом выражении можно пренебречь
+        и считать число операций примерно равным N × K.
+        */
         for (let i = beginIndex; i < endIndex; i++) {
-            currentSum += timeseries[i];
+            currentSum += timeSeries[i];
         }
         let currentAvg = currentSum / K;
         result.push(currentAvg);
@@ -15,14 +20,14 @@ function movingAverage(timeseries, K) {
 
 console.log(movingAverage([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 3));
 
-function movingAverageTwoPointers(timeseries, K) {
+function movingAverageTwoPointers(timeSeries, K) {
     let result = []; // Empty array.
     // Calculate the first value honestly and save the result.
-    let current_sum = timeseries.slice(0, K).reduce((sum, val) => sum + val, 0);
+    let current_sum = timeSeries.slice(0, K).reduce((sum, val) => sum + val, 0);
     result.push(current_sum / K); // Add to result.
-    for (let i = 0; i < timeseries.length - K; i++) {
-        current_sum -= timeseries[i];
-        current_sum += timeseries[i + K];
+    for (let i = 0; i < timeSeries.length - K; i++) {
+        current_sum -= timeSeries[i];
+        current_sum += timeSeries[i + K];
         const current_avg = current_sum / K;
         result.push(current_avg); // Add to result.
     }
@@ -30,3 +35,18 @@ function movingAverageTwoPointers(timeseries, K) {
 }
 
 console.log(movingAverageTwoPointers([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 3));
+
+function slidingWindow(timeSeries, K) {
+    let result = []; // Empty array.
+    // Calculate the first value honestly and save the result.
+    let current_sum = timeSeries.slice(0, K).reduce((sum, val) => sum + val, 0);
+    result.push(current_sum / K); // Add to result.
+    for (let i = K; i < timeSeries.length; i++) {
+        current_sum += timeSeries[i] - timeSeries[i - K];
+        const current_avg = current_sum / K;
+        result.push(current_avg); // Add to result.
+    }
+    return result;
+}
+
+console.log(slidingWindow([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 3));
