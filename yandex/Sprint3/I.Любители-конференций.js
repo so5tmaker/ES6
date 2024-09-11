07943681272a9484cbbb1b9e0ec3fe57dbeadf96
Viktor Sivulskiy <info@softmaker.kz>
2024-09-11 07:08:43 +0500

    sprint 3 i conference lovers

---
+++ yandex/Sprint3/I.Любители-конференций.js
@@ -0,0 +1,34 @@
+const _readline = require('readline');
+
+const _reader = _readline.createInterface({
+    input: process.stdin
+});
+
+const _inputLines = [];
+let _curLine = 0;
+
+// Установим callback на считывание строки - так мы получим
+// все строки из ввода в массиве _inputLines.
+_reader.on('line', line => {
+    _inputLines.push(line);
+});
+
+// Когда ввод закончится, будет вызвана функция solve.
+process.stdin.on('end', solve);
+
+function readNumber() {
+    return Number(_inputLines[_curLine++]);
+}
+
+// Функция парсит число из очередной строки массива _inputLines
+// и сдвигает указатель на единицу вперёд.
+function readArrayNumber() {
+    return _inputLines[_curLine++].split(' ').map(i => Number(i));
+}
+
+function solve() {
+    const n = readNumber();
+    const k = readArrayNumber().sort((a, b) => b - a); // убывание должно быть desc
+
+    console.log();
+}
