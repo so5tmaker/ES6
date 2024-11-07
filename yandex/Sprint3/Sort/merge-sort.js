3f819f30ddebe4dbef81011e6e4ccdda3941c3c7
Viktor Sivulskiy <info@softmaker.kz>
2024-11-07 12:30:10 +0500

    sprint 3 merge sort

--- yandex/Sprint3/Sort/merge-sort.js
+++ yandex/Sprint3/Sort/merge-sort.js
@@ -41,25 +41,3 @@ function mergeSort(array) {
 
     return result;
 }
-
-console.log('1');
-
-setTimeout(function () {
-    console.log('2');
-    Promise.resolve().then(function () {
-        console.log('3');
-    }, 0);
-}, 0);
-
-Promise.resolve().then(function () {
-    console.log('4');
-    setTimeout(function () {
-        console.log('5');
-    }, 0);
-});
-
-requestAnimationFrame(function () {
-    console.log('7');
-});
-
-console.log('6');
