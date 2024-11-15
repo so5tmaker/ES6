6c9e886e72beea8c89b3f6c4a5b997f280be9ab5
Viktor Sivulskiy <info@softmaker.kz>
2024-11-15 07:33:48 +0500

    sprint 1 l extra letter intro

---
+++ yandex/Sprint1/L.Лишняя-буква.js
@@ -0,0 +1,30 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+const LINE = 'line';
+const CLOSE = 'close';
+
+const inputLines = [];
+let curLine = 0;
+
+// чтение строк из стандартного ввода
+reader.on(LINE, line => inputLines.push(line));
+reader.on(CLOSE, solve);
+
+// функция для чтения строк из входных данных
+const readNumber = () => Number(inputLines[curLine++]);
+
+const getNumber = () => inputLines[curLine++].split(' ').map(Number);
+
+function solve() {
+    const n = readNumber();
+    const x = getNumber();
+
+    console.log(n);
+    console.log(x);
+}
